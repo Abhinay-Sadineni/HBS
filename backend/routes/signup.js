4e527bd83c323ec2006be656a092812fe7116162
Kotikalapudi Karthik <cs21btech11030@iith.ac.in>
2024-04-06 22:14:08 +0530

    Modified login, signup

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
-const auth = require('../middlewares/auth');
 
 const SignupRouter = express.Router();
 
-SignupRouter.post('/signup', auth, async(req, res) => {
+SignupRouter.post('/signup', async(req, res) => {
     try {
         const {username, email, password, phone_number, country_code} = req.body;
 
@@ -20,17 +18,14 @@ SignupRouter.post('/signup', auth, async(req, res) => {
         }
 
         const hashed_password = await bcrypt.hash(password, 10);
-        const newUser = await User.create({
+        await User.create({
             username,
             email,
             password: hashed_password,
             phone_number,
             country_code
         });
-
-        const token = jwt.sign({user_id: newUser.user_id}, process.env.SECRET_KEY);
-
-        res.status(201).json({token});
+        res.status(201).json({"message": "Account Created"});
 
     } catch (error) {
         console.error(error);
