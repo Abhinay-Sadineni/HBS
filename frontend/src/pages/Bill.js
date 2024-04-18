8a34cfb2df23df501d96943d6eb6b060c3671c74
Sriramjupally <cs21btech11025@iith.ac.in>
2024-04-18 22:21:54 +0530

    Completed Bill Page

--- frontend/src/pages/Bill.js
+++ frontend/src/pages/Bill.js
@@ -6,22 +6,61 @@ import hotelsList from '../components/hotels';
 
 function BillPage() {
   return (
-    <div className="HI">
+    <div className="HI flex flex-col items-center">
       <div className="fixed top-0 w-full z-10 mb-10 border-b border-gray-300">
         <NavBar />
         {/* <SearchBar /> */}
       </div>
       <div className="m-10"> <p>h</p> </div>
-      <h1>Hi</h1>
-      <div className = "bg-white shadow-lg border-gray-900"> 
-        <div className="flex">
-            <div>
-            <h1 className="text-3xl font-bold">Booking Id</h1>
+      <div className="bg-white shadow-lg border-t border-gray-100 w-4/5 ">
+        <div className="flex border-b border-gray-300  p-6">
+          <div>
+            <h1 className="text-xl font-bold">Booking Id</h1>
             <p className="text-lg">XYCC094</p>
-            </div>
-            <div className="ml-auto">
+          </div>
+          <div className="ml-auto">
             <p>Booked by UserName on DateandTime</p>
+          </div>
+        </div>
+        <div className="flex border-b border-gray-300  p-4 items-center">
+          <div>
+            <h1 className="text-xl font-bold">Hotel Name</h1>
+            <p>Hotel Location</p>
+          </div>
+          <div className="ml-auto">
+            <img src={hotelsList[0].imgURL} className="w-72 h-auto" />
+          </div>
+        </div>
+        <div className="flex justify-between border-b border-gray-300 p-4 items-center">
+          <div className="flex-grow">
+            <h1 className="text-l font-bold">Primary Guest</h1>
+            <p>UserName</p>
+          </div>
+          <div className="flex-grow">
+            <h1 className="text-l font-bold">Check in</h1>
+            <p>CheckInTime</p>
+          </div>
+          <div className="flex-grow">
+            <h1 className="text-l font-bold">Check Out</h1>
+            <p>CheckOutTime</p>
+          </div>
+          <div className="ml-auto">
+            <h1 className="text-3xl font-bold">1 Night</h1>
+            <div className="flex">
+              <p>1 Guest</p>
+              <div className="ml-4">
+                <p>1 Room</p>
+                <p>Classic</p>
+              </div>
             </div>
+          </div>
+        </div>
+        <div className="p-4">
+          <h1 className="text-xl font-bold">Payment Details</h1> 
+          <div className="flex  border border-gray-300 p-4 m-2">
+            <p>Total payable amount</p>
+            <h2 className="text-l font-bold ml-auto">724Rs</h2>
+          </div>
         </div>
       </div>
     </div>
@@ -29,4 +68,7 @@ function BillPage() {
 }
 
 export default BillPage;
+
+
+
 
