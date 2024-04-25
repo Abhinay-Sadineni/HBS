5739fed6fd2e26fc3240fcb21f04d7712d695278
Sriramjupally <cs21btech11025@iith.ac.in>
2024-04-25 22:12:45 +0530

    updated hotels.js

--- frontend/src/components/hotels.js
+++ frontend/src/components/hotels.js
@@ -52,7 +52,68 @@ const hotelsList = [
         location : "Kolkata",
         rating : "4.2",
         price: "1500Rs"
-    }
+    },
+    {
+      id: 7,
+      imgURL : ["https://a0.muscache.com/im/pictures/ae71d719-bb9b-466d-aa76-c29a1d43afd7.jpg?im_w=720"],
+      name: "Hotel 6",
+      location : "Kolkata",
+      rating : "4.2",
+      price: "1500Rs"
+  },
+  {
+    id: 8,
+    imgURL : ["https://a0.muscache.com/im/pictures/miso/Hosting-9211435/original/544ef703-7834-43d7-95d6-af2f2d1aa21f.jpeg?im_w=720"],
+    name: "Hotel 5",
+    location : "Banglore",
+    rating : "4.2",
+    price: "1500Rs"
+},
+{
+  id: 9,
+  imgURL : ["https://a0.muscache.com/im/pictures/miso/Hosting-820733145568572294/original/0c68a135-b239-4a95-b3d6-ad89816cd922.jpeg?im_w=720"],
+  name: "Hotel 4",
+  location : "Chennai",
+  rating : "4.2",
+  price: "1500Rs"
+},
+{
+  id: 10,
+  imgURL : ["https://a0.muscache.com/im/pictures/2a1724ca-c902-4c82-b64a-215c2fd1e414.jpg?im_w=720"],
+  name: "Hotel 3",
+  location : "Delhi",
+  rating : "4.2",
+  price: "1500Rs"
+},
+{
+  id: 11,
+  imgURL : [
+   "https://a0.muscache.com/im/pictures/14152ff7-28fa-48cc-9c90-ac787fb5bb6b.jpg?im_w=720",
+   "https://a0.muscache.com/im/pictures/miso/Hosting-3251026/original/825da780-127b-4155-982b-2a958b436eb2.jpeg?im_w=720",
+   "https://a0.muscache.com/im/pictures/a246b08e-5a1d-46f9-a75a-c4392fd670a7.jpg?im_w=720",
+   "https://a0.muscache.com/im/pictures/ea3a10aa-645f-4754-a4a2-9a4920b2c17b.jpg?im_w=720",
+   "https://a0.muscache.com/im/pictures/7c6b0039-39d1-4d45-b405-898481cd12c2.jpg?im_w=720",
+  ],
+  name: "Hotel 1",
+  location : "Hyderabad",
+  rating : "4.2",
+  price: "1500Rs"
+},
+
+{
+  id: 12,
+  imgURL : [
+   "https://a0.muscache.com/im/pictures/14152ff7-28fa-48cc-9c90-ac787fb5bb6b.jpg?im_w=720",
+   "https://a0.muscache.com/im/pictures/miso/Hosting-3251026/original/825da780-127b-4155-982b-2a958b436eb2.jpeg?im_w=720",
+   "https://a0.muscache.com/im/pictures/a246b08e-5a1d-46f9-a75a-c4392fd670a7.jpg?im_w=720",
+   "https://a0.muscache.com/im/pictures/ea3a10aa-645f-4754-a4a2-9a4920b2c17b.jpg?im_w=720",
+   "https://a0.muscache.com/im/pictures/7c6b0039-39d1-4d45-b405-898481cd12c2.jpg?im_w=720",
+  ],
+  name: "Hotel 1",
+  location : "Hyderabad",
+  rating : "4.2",
+  price: "1500Rs"
+}
 
   ];
   
