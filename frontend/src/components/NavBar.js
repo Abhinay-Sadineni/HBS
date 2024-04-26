50d3409ec8afe96ddc733b4c1ee088e7b2316b54
abhinay sadineni <96472013+karna-rash@users.noreply.github.com>
2024-04-26 22:28:43 +0530

    add navbar change

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/HBSLogo.jpeg';
 import SearchBar from './SearchBar';
+import axiosInstance from '../helpers/axios'
 
 import {
   Menu,
@@ -13,38 +14,90 @@ import {
 import { FaUser, FaSignOutAlt, FaQuestionCircle } from 'react-icons/fa';
 import { FiAlignJustify } from "react-icons/fi";
 
-
 function NavBar() {
-  return(
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const response = axiosInstance.get('./login')
+        .then((response)=>{
+          if(response.status == 200){
+                setLoading(true)
+                setIsLoggedIn(true)
+                setIsLoggedIn(false)
+          }
+          else{
+
+          }
+        })
+      } catch (error) {
+        console.error('Error checking token:', error);
+        setIsLoggedIn(false); 
+      } finally {
+        setLoading(false); 
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // Render a loading indicator while waiting for the response
+  }
+
+  return (
     <nav className='fixed z-1000 top-0 left-0 shadow-[rgba(0,0,15,0.5)_2px_2px_2px_0px] w-full bg-white-800 opacity-100'>
-        <div className='flex flex-row items-center justify-between'>
-          <img src={logo} className='rounded-full px-2 py-2' style={{ height: '75px', width: '75px' }} alt="HBS Logo" />
-          <SearchBar />
-          <div className='mx-4'>
-            <Menu>
-              <MenuHandler>
-                <button className='flex px-4 py-4 items-center'>
-                  <FiAlignJustify className='mx-2' /> Menu
-                </button>
-              </MenuHandler>
-              <MenuList className='w-max'>
-                <MenuItem >
-                <Link to="/profile" className="flex items-center px-4 py-4" >
-                  <FaUser className='mr-2' /> Profile
-                </Link>
-                </MenuItem>
-                <MenuItem className='flex px-4 py-4 items-center'>
-                  <FaSignOutAlt className='mr-2' /> Sign Out
-                </MenuItem>
-                <MenuItem className='flex px-4 py-4 items-center '>
-                  <FaQuestionCircle className='mr-2' /> Help Center
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          </div>
+      <div className='flex flex-row items-center justify-between'>
+        <img src={logo} className='rounded-full px-2 py-2' style={{ height: '75px', width: '75px' }} alt="HBS Logo" />
+        <SearchBar />
+        <div className='mx-4'>
+          <Menu>
+            <MenuHandler>
+              <button className='flex px-4 py-4 items-center'>
+                <FiAlignJustify className='mx-2' /> Menu
+              </button>
+            </MenuHandler>
+            <MenuList className='w-max'>
+              {!isLoggedIn ? (
+                <>
+                  <MenuItem>
+                    <Link to="/profile" className="flex items-center px-4 py-4" >
+                      <FaUser className='mr-2' /> Profile
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link to="/history" className="flex items-center px-4 py-4" >
+                      <FaUser className='mr-2' /> History
+                    </Link>
+                  </MenuItem>
+                  <MenuItem className='flex px-4 py-4 items-center'>
+                    <FaSignOutAlt className='mr-2' /> Sign Out
+                  </MenuItem>
+                </>
+              ) : (
+                <>
+                  <MenuItem>
+                    <Link to="/login" className="flex items-center px-4 py-4" >
+                      <FaUser className='mr-2' /> Login
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link to="/signup" className="flex items-center px-4 py-4" >
+                      <FaUser className='mr-2' /> Signup
+                    </Link>
+                  </MenuItem>
+                </>
+              )}
+              <MenuItem className='flex px-4 py-4 items-center '>
+                <FaQuestionCircle className='mr-2' /> Help Center
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </div>
-      </nav>
- 
+      </div>
+    </nav>
   )
 }
 
