28d6374f3974a7051a77f819a713d6041b52675f
Sriramjupally <cs21btech11025@iith.ac.in>
2024-04-29 10:38:06 +0530

    updated calendar to work with roomTypes and signOut manager

--- frontend/src/components/Manager_navbar.js
+++ frontend/src/components/Manager_navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { Link ,useLocation ,useNavigate} from 'react-router-dom';
 import logo from '../assets/images/HBSLogo.jpeg';
 
 import {
@@ -14,8 +14,32 @@ import { FiAlignJustify } from "react-icons/fi";
 
 
 function Manager_NavBar() {
+  const [loading, setLoading] = useState(true);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleSignout =  () => {
+    try {
+      console.log("trying to logout")
+      setLoading(true);
+      localStorage.removeItem('token');
+      // setIsLoggedIn(false);
+    } catch (error) {
+      console.error('Error signing out:', error);
+    } finally {
+      const pathsToRedirect = ['/manager-dashboard', '/manager-calendar', '/manager-hotel', '/manager-reservations'];
+      if (pathsToRedirect.includes(location.pathname)) {
+        navigate('/');
+      }
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000); 
+    }
+  }
+
   return(
-    <nav className='fixed top-0 left-0 shadow-[rgba(0,0,15,0.5)_2px_2px_2px_0px] w-full bg-white-800 opacity-100'>
+    <nav className='fixed z-1000 top-0 left-0 shadow-[rgba(0,0,15,0.5)_2px_2px_2px_0px] w-full bg-white bg-opacity-99'>
         <div className='flex flex-row items-center justify-between'>
           <img src={logo} className='rounded-full px-2 py-2' style={{ height: '75px', width: '75px' }} alt="HBS Logo" />
           <div className='flex items-center justify-between'>
@@ -38,7 +62,7 @@ function Manager_NavBar() {
                 </Link>
                 </MenuItem>
                 <MenuItem className='flex px-4 py-4 items-center'>
-                  <FaSignOutAlt className='mr-2' /> Sign Out
+                   <button className='mr-2 flex px-4 py-4 items-center' onClick={handleSignout} > Sign Out</button>
                 </MenuItem>
                 <MenuItem className='flex px-4 py-4 items-center '>
                   <FaQuestionCircle className='mr-2' /> Help Center
